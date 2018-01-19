16ee7040a17ffa246858054404bdeb6f9d107d3f
krgauraw <er.krgauraw@gmail.com>
2018-01-19 17:37:58 +0530

    Issue #1 feat: App1

---
+++ Hibernate/HIB_L_App1/src/main/java/dev/hib/person/impl/PersonImpl.java
@@ -0,0 +1,28 @@
+package dev.hib.person.impl;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import dev.hib.person.pojo.Person;
+
+public class PersonImpl {
+	public static void main(String[] args) {
+
+		Person p1 = new Person();
+		p1.setfName("Kumar");
+		p1.setlName("Gauraw");
+		p1.setAge(24);
+
+		Configuration c1 = new Configuration();
+		c1.configure();
+		SessionFactory sf = c1.buildSessionFactory();
+		Session s1 = sf.openSession();
+		s1.beginTransaction();
+		String val = (String) s1.save(p1);
+		s1.getTransaction().commit();
+		s1.flush();
+		s1.close();
+		System.out.println("Record Inserted Successfully for : " + val);
+	}
+}
